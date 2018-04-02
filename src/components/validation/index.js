956eeb6de4217d7cd124ec5b9cb9e004096a85dc
Chris Heninger <chris.heninger@knledg.com>
2018-04-02 08:38:23 -0700

    Update validation with new alert colors

--- src/components/validation/index.js
+++ src/components/validation/index.js
@@ -2,19 +2,27 @@ import React from 'react';
 import withStyles from 'react-jss';
 import classNames from 'classnames';
 
-const Validation = ({ className, classes, error, theme, ...props }) => (
+const Validation = ({ className, classes, color, error, theme, ...props }) => (
   <div className={classNames(classes.root, className)} {...props}>
     {error}
   </div>
 );
+
+Validation.defaultProps = {
+  color: 'danger',
+};
 
 export default withStyles(theme => ({
   root: {
     display: 'inline-flex',
     fontSize: '14px',
     lineHeight: '18px',
-    color: theme.colors.white,
-    backgroundColor: theme.colors.danger,
+    color: props => theme.alertColors[props.color].color,
+    backgroundColor: props => theme.alertColors[props.color].backgroundColor,
+    borderRadius: '2px',
+    borderColor: props => theme.alertColors[props.color].borderColor,
+    borderWidth: '1px',
+    borderStyle: 'solid',
     marginTop: '4px',
     padding: '2px 8px',
   },
