79398d1134c5b3b4f8f1c85c2e1cf46f3e58a1b0
Max Korp <maxkorp@8bytealchemy.com>
2018-05-08 11:17:45 -0700

    add yarn clean command and beef up gitignore

---
+++ scripts/clean.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const rimraf = require('rimraf');
+
+let good = 0;
+let bad = 0;
+fs.readdirSync('src').forEach(name => {
+  try {
+    rimraf.sync(name);
+    good++;
+  } catch (e) {
+    bad++;
+    console.log(`couldn't delete ${name}`);
+  }
+});
+
+console.log(
+  `successfully cleaned ${good} out of ${good + bad} entries${
+    bad ? '; ' + bad + ' failures' : ''
+  }`
+);
+
+process.exit(bad);
