27de40bc045e31537f4f7e74614ac4ba108c513b
Chris Heninger <chris.heninger@knledg.com>
2018-05-14 14:32:02 -0700

    Style updates and demo Table component

--- demo/src/docs/Tables.js
+++ demo/src/docs/Tables.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import Paper from 'elevate-ui/Paper';
+import Table from 'elevate-ui/Table';
 import Typography from 'elevate-ui/Typography';
 
-function Home() {
+function Tables() {
+  const columns = [
+    {
+      Header: 'Name',
+      accessor: 'name',
+    },
+    {
+      Header: 'Animal',
+      accessor: 'animal',
+    },
+    {
+      Header: 'Food',
+      accessor: 'food',
+    },
+    {
+      Header: 'Start Date',
+      accessor: 'startDate',
+    },
+  ];
+  const data = [
+    {
+      name: 'Chris Heninger',
+      animal: 'Cat',
+      food: 'Pizza',
+      startDate: '2018-01-20',
+    },
+    {
+      name: 'Jason Walsh',
+      animal: 'Red Panda',
+      food: 'Swedish Fish',
+      startDate: '2018-01-20',
+    },
+    {
+      name: 'Matt Lorey',
+      animal: 'Cat',
+      food: 'Steak',
+      startDate: '2018-01-20',
+    },
+    {
+      name: 'Eric Vinson',
+      animal: 'Panda',
+      food: 'Pho',
+      startDate: '2018-01-20',
+    },
+    {
+      name: 'Ashley Fogarty',
+      animal: 'Red Fox',
+      food: 'Buffalo Chicken',
+      startDate: '2018-01-20',
+    },
+    {
+      name: 'Maximiliano Korp',
+      animal: 'Fox',
+      food: 'Paella',
+      startDate: '2018-01-20',
+    },
+  ];
+
   return (
     <Paper>
       <Typography type="title">{`<Table />`}</Typography>
       <Typography type="body">
-        The {`<Table />`} component is a styled wrapper around the lightweight,
-        fast, and extendable datagrid [React-Table](https://react-table.js.org).
+        The {`<Table />`} component is a styled wrapper around the open-source,
+        datagrid{' '}
+        <a
+          href="https://react-table.js.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React-Table
+        </a>.
       </Typography>
+      <Table data={data} columns={columns} />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/react-table@latest/react-table.css"
+      />
     </Paper>
   );
 }
 
-export default Home;
+export default Tables;
