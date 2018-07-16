1a63a4c97444eee64127ded1126bd945c5d71825
Chris Heninger <chris.heninger@knledg.com>
2018-07-16 13:34:03 -0700

    Added some icons

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,27 +1,39 @@
 import React from "react";
 
+import Bath from "./Bath";
+import Bed from "./Bed";
 import Broken from "./Broken";
 import Calendar from "./Calendar";
 import Cart from "./Cart";
 import CreditCard from "./CreditCard";
 import Coupon from "./Coupon";
 import ExclamationOutline from "./ExclamationOutline";
+import Heart from "./Heart";
 import Kebab from "./Kebab";
 import Person from "./Person";
 import Refresh from "./Refresh";
+import Save from "./Save";
+import SqFt from "./SqFt";
 import Times from "./Times";
+import Video from "./Video";
 
 const icons = {
+  Bath,
+  Bed,
   Broken,
   Calendar,
   Cart,
   CreditCard,
   Coupon,
   ExclamationOutline,
+  Heart,
   Kebab,
   Person,
   Refresh,
+  Save,
+  SqFt,
   Times,
+  Video,
 };
 
 const Icon = ({ icon, ...rest }) => {
