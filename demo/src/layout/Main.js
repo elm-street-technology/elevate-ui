28a8f8ab8e7dcac7e2e038d4bb7f9932d2f62e1e
Eric Stout <estout@elmstreettechnology.com>
2018-07-31 14:54:18 -0700

    Add scroll to top to fix issue #64

--- demo/src/layout/Main.js
+++ demo/src/layout/Main.js
@@ -4,6 +4,7 @@ import withStyles from "react-jss";
 
 import Loadable from "elevate-ui/Loadable";
 
+import RouteListener from "./RouteListener";
 import Sidebar from "./Sidebar";
 import Doc from "./Doc";
 import IconDoc from "./IconDoc";
@@ -12,30 +13,35 @@ const Home = Loadable({ loader: () => import("../docs/Home") });
 const SignupForm = Loadable({ loader: () => import("../docs/SignupForm") });
 
 const Main = ({ classes }) => (
-  <div className={classes.root}>
-    <div className={classes.sidebar}>
-      <Sidebar />
+  <RouteListener>
+    <div className={classes.root}>
+      <div className={classes.sidebar}>
+        <Sidebar />
+      </div>
+      <div className={classes.children}>
+        <Route exact path="/" component={Home} />
+        <Route path="/input" render={() => <Doc folder="Input" />} />
+        <Route path="/textarea" render={() => <Doc folder="Textarea" />} />
+        <Route path="/select" render={() => <Doc folder="Select" />} />
+        <Route
+          path="/multi-select"
+          render={() => <Doc folder="MultiSelect" />}
+        />
+        <Route path="/checkbox" render={() => <Doc folder="Checkbox" />} />
+        <Route
+          path="/checkbox-group"
+          render={() => <Doc folder="CheckboxGroup" />}
+        />
+        <Route path="/radio-group" render={() => <Doc folder="RadioGroup" />} />
+        <Route path="/datetime" render={() => <Doc folder="Datetime" />} />
+        <Route path="/button" render={() => <Doc folder="Button" />} />
+        <Route path="/alert" render={() => <Doc folder="Alert" />} />
+        <Route path="/table" render={() => <Doc folder="Table" />} />
+        <Route path="/icon" render={() => <IconDoc />} />
+        <Route path="/signup" component={SignupForm} />
+      </div>
     </div>
-    <div className={classes.children}>
-      <Route exact path="/" component={Home} />
-      <Route path="/input" render={() => <Doc folder="Input" />} />
-      <Route path="/textarea" render={() => <Doc folder="Textarea" />} />
-      <Route path="/select" render={() => <Doc folder="Select" />} />
-      <Route path="/multi-select" render={() => <Doc folder="MultiSelect" />} />
-      <Route path="/checkbox" render={() => <Doc folder="Checkbox" />} />
-      <Route
-        path="/checkbox-group"
-        render={() => <Doc folder="CheckboxGroup" />}
-      />
-      <Route path="/radio-group" render={() => <Doc folder="RadioGroup" />} />
-      <Route path="/datetime" render={() => <Doc folder="Datetime" />} />
-      <Route path="/button" render={() => <Doc folder="Button" />} />
-      <Route path="/alert" render={() => <Doc folder="Alert" />} />
-      <Route path="/table" render={() => <Doc folder="Table" />} />
-      <Route path="/icon" render={() => <IconDoc />} />
-      <Route path="/signup" component={SignupForm} />
-    </div>
-  </div>
+  </RouteListener>
 );
 
 export default withStyles((theme) => ({
