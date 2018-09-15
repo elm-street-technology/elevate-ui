08b409aa3fbfaf977895e3ee4ecca1629a2b3d0d
Eric Stout <ericwstout@gmail.com>
2018-09-15 13:23:06 -0700

    Add tests

---
+++ src/ButtonGroup/ButtonGroup.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import sinon from "sinon";
+
+import ThemeProvider from "../ThemeProvider";
+import ButtonGroup from "./";
+
+const WrappedGroup = (props) => (
+  <ThemeProvider>
+    <ButtonGroup {...props} />
+  </ThemeProvider>
+);
+
+test("renders without crashing, matches the snapshot", () => {
+  const component = renderer.create(
+    <WrappedGroup
+      id="test-buttongroup"
+      label="Test ButtonGroup"
+      onChange={sinon.spy()}
+      field={{
+        name: "test-checkybox",
+        onChange: () => {},
+        onBlur: () => {},
+        value: false,
+      }}
+      form={{
+        errors: [],
+        setFieldValue: () => {},
+        setFieldTouched: () => {},
+        touched: false,
+      }}
+    />
+  );
+  const componentJSON = component.toJSON();
+  expect(componentJSON).toMatchSnapshot();
+});
