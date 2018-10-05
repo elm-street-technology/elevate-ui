47555dcb9cea96c4923a7ebfee3d016b480726c2
Eric Stout <ericwstout@gmail.com>
2018-10-05 10:49:15 -0700

    Update typography component and add documentation

---
+++ demo/src/docs/Typography.js
@@ -0,0 +1,68 @@
+// @flow
+import React from "react";
+import Paper from "elevate-ui/Paper";
+import Typography from "elevate-ui/Typography";
+
+const Typographies = () => (
+  <Paper>
+    <Typography type="title" gutterBottom>
+      This Is A Title
+    </Typography>
+    <Typography type="body" gutterBottom>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id euismod
+      nibh, et efficitur nibh. Aenean cursus auctor tortor sed porta.
+      Suspendisse ipsum metus, sodales porttitor dictum a, vehicula eu leo.
+      Donec auctor nisi auctor tellus egestas commodo. Nunc auctor, justo eu
+      rutrum auctor, magna elit maximus enim, a vulputate ipsum eros non magna.
+      Etiam varius egestas dapibus. Curabitur eu vestibulum quam, ut mattis
+      neque. Sed neque risus, finibus eu volutpat sed, fringilla sed nisl.
+    </Typography>
+    <Typography type="heading2" gutterBottom>
+      Heading 2
+    </Typography>
+    <Typography type="body" gutterBottom>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id euismod
+      nibh, et efficitur nibh. Aenean cursus auctor tortor sed porta.
+      Suspendisse ipsum metus, sodales porttitor dictum a, vehicula eu leo.
+      Donec auctor nisi auctor tellus egestas commodo. Nunc auctor, justo eu
+      rutrum auctor, magna elit maximus enim, a vulputate ipsum eros non magna.
+      Etiam varius egestas dapibus. Curabitur eu vestibulum quam, ut mattis
+      neque. Sed neque risus, finibus eu volutpat sed, fringilla sed nisl.
+    </Typography>
+    <Typography type="heading3" gutterBottom>
+      Heading 3
+    </Typography>
+    <Typography type="body" gutterBottom>
+      Nullam justo felis, rutrum eget accumsan id, iaculis vel velit. Aliquam
+      pretium varius purus, sed imperdiet turpis commodo quis. Nam semper, dui a
+      egestas posuere, eros nulla hendrerit purus, et porttitor sapien mauris at
+      nunc. Cras id nisi tincidunt, egestas ligula a, posuere augue. Proin sit
+      amet facilisis nisl. Maecenas ut enim eget elit molestie faucibus quis sed
+      eros. Curabitur a lacus egestas, posuere quam mollis, pharetra est.
+    </Typography>
+    <Typography type="heading4" gutterBottom>
+      Heading 4
+    </Typography>
+    <Typography type="body" gutterBottom>
+      Nam semper, dui a egestas posuere, eros nulla hendrerit purus, et
+      porttitor sapien mauris at nunc. Cras id nisi tincidunt, egestas ligula a,
+      posuere augue. Proin sit amet facilisis nisl.
+    </Typography>
+    <Typography type="heading5" gutterBottom>
+      Heading 5
+    </Typography>
+    <Typography type="body" gutterBottom>
+      Nam semper, dui a egestas posuere, eros nulla hendrerit purus, et
+      porttitor sapien mauris at nunc. Cras id nisi tincidunt, egestas ligula a,
+      posuere augue. Proin sit amet facilisis nisl.
+    </Typography>
+    <Typography type="heading6" gutterBottom>
+      Heading 6
+    </Typography>
+    <Typography type="title" component="span">
+      With component override
+    </Typography>
+  </Paper>
+);
+
+export default Typographies;
