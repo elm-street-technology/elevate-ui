cf9268d34673aecb29906b80bbeee27de7ffc567
Eric Stout <ericwstout@gmail.com>
2018-10-05 09:31:52 -0700

    Allow Custom Value "Tags" in MultiSelect (#106)

--- demo/src/docs/MultiSelect.js
+++ demo/src/docs/MultiSelect.js
@@ -7,38 +7,47 @@ import MultiSelect from "elevate-ui/MultiSelect";
 import Button from "elevate-ui/Button";
 
 const roygbiv = [
-  { label: "Red", value: "red" },
-  { label: "Orange", value: "orange" },
-  { label: "Yellow", value: "yellow" },
-  { label: "Green", value: "green" },
-  { label: "Blue", value: "blue" },
-  { label: "Indigo", value: "indigo" },
-  { label: "Violet", value: "violet" },
+  { label: "Red", value: "Red" },
+  { label: "Orange", value: "Orange" },
+  { label: "Yellow", value: "Yellow" },
+  { label: "Green", value: "Green" },
+  { label: "Blue", value: "Blue" },
+  { label: "Indigo", value: "Indigo" },
+  { label: "Violet", value: "Violet" },
 ];
 const cmyk = [
-  { label: "Cyan", value: "cyan" },
-  { label: "Magenta", value: "magenta" },
-  { label: "Yellow", value: "yellow" },
-  { label: "Black", value: "black" },
+  { label: "Cyan", value: "Cyan" },
+  { label: "Magenta", value: "Magenta" },
+  { label: "Yellow", value: "Yellow" },
+  { label: "Black", value: "Black" },
 ];
 
-const MultiSelects = ({
-  values,
-  errors,
-  touched,
-  handleChange,
-  handleBlur,
-  handleSubmit,
-  isSubmitting,
-}) => (
+const cities = [
+  { label: "Phoenix", value: "Phoenix" },
+  { label: "Tempe", value: "Tempe" },
+  { label: "Chandler", value: "Chandler" },
+  { label: "Scottsdale", value: "Scottsdale" },
+];
+
+const MultiSelects = () => (
   <Paper>
     <Formik
-      initialValues={{ colors: [], colors2: ["magenta"] }}
+      initialValues={{
+        colors: [],
+        colors2: ["Magenta"],
+        cities: [],
+      }}
       validationSchema={() =>
         Yup.object().shape({
           colors: Yup.array()
             .of(Yup.string())
             .required("A color is required"),
+          colors2: Yup.array()
+            .of(Yup.string())
+            .required("A color is required"),
+          cities: Yup.array()
+            .of(Yup.string())
+            .required("A city is required"),
         })
       }
       onSubmit={(values, { setSubmitting }) => {
@@ -47,16 +56,7 @@ const MultiSelects = ({
           setSubmitting(false);
         }, 1000);
       }}
-      render={({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        isValid,
-      }) => (
+      render={({ isSubmitting, isValid }) => (
         <Form noValidate style={{ maxWidth: "420px" }}>
           <Field
             id="colors"
@@ -72,6 +72,14 @@ const MultiSelects = ({
             items={cmyk}
             component={MultiSelect}
             closeOnSelect
+          />
+          <Field
+            id="cities"
+            name="cities"
+            label="Cities (with custom tags allowed)"
+            items={cities}
+            component={MultiSelect}
+            tags={true}
           />
           <Button type="submit" disabled={!isValid || isSubmitting}>
             Submit
