b6af68750fb81eeee25ff498867bc091adb41780
Eric Stout <ericwstout@gmail.com>
2018-10-05 11:02:41 -0700

    Add typography tests

---
+++ src/Typography/Typography.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import ThemeProvider from "../ThemeProvider";
+import Typography from "./";
+
+const WrappedTypographies = () => (
+  <ThemeProvider>
+    <Typography type="title">Lorem Ipsum Dolor Sit Amet</Typography>
+    <Typography type="body">Lorem Ipsum Dolor Sit Amet</Typography>
+    <Typography type="heading2" gutterBottom>
+      Lorem Ipsum Dolor Sit Amet
+    </Typography>
+    <Typography type="heading3">Lorem Ipsum Dolor Sit Amet</Typography>
+    <Typography type="heading4">Lorem Ipsum Dolor Sit Amet</Typography>
+    <Typography type="heading5">Lorem Ipsum Dolor Sit Amet</Typography>
+    <Typography type="heading6">Lorem Ipsum Dolor Sit Amet</Typography>
+    <Typography type="title" component="span">
+      Lorem Ipsum Dolor Sit Amet
+    </Typography>
+  </ThemeProvider>
+);
+
+test("renders without crashing, matches the snapshot", () => {
+  const component = renderer.create(<WrappedTypographies />);
+  const componentJSON = component.toJSON();
+  expect(componentJSON).toMatchSnapshot();
+});
