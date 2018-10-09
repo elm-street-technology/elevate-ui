d1c9922fa4fd4a0c647cb3949e438284b08d6d1e
Eric Stout <ericwstout@gmail.com>
2018-10-09 08:17:50 -0700

    Add additional headings to Typography, add Typography to the docs (#107)
    * Allow Custom Value "Tags" in MultiSelect (#106)
    
    * Update component and component json
    
    * Update typography component and add documentation
    
    * Add typography tests
    
    * Add gutterTop, update header colors
    
    * update body copy default color
    
    * Update component and component json
    
    * Update typography component and add documentation
    
    * Add typography tests
    
    * Add gutterTop, update header colors
    
    * update body copy default color
    
    * 🤷‍♂️
    
    * remove flow
    
    * Update snapshot for Multiselect
    
    * Yarn lock updates
    
    * whyyyyyy

--- src/Typography/index.js
+++ src/Typography/index.js
@@ -8,21 +8,35 @@ type Props = {
   /**
    * Accepts any classNames to be passed down to the component.
    */
-  className: string,
+  className?: string,
   /**
-   * What HTML element or component to use when rendered.
+   * What HTML element to use when rendered.
    */
-  component: string,
+  element?: string,
+  /**
+   * Adds margin to the top of the component
+   */
+  gutterTop?: boolean,
+  /**
+   * Adds margin to the bottom of the component
+   */
+  gutterBottom?: boolean,
   theme: Object,
   /**
-   * Type of typography to render such as "title" or "body".
+   * Type of text to be used.
    */
-  type: "title" | "body",
+  // prettier-ignore
+  type: "title"| "body"| "heading2"| "heading3"| "heading4"| "heading5"| "heading6"
 };
 
 const typeElementMap = {
   title: "h1",
   body: "p",
+  heading2: "h2",
+  heading3: "h3",
+  heading4: "h4",
+  heading5: "h5",
+  heading6: "h6",
 };
 
 /**
@@ -32,15 +46,23 @@ const Typography = ({
   children,
   classes,
   className,
-  component: componentProp,
+  element: elementProp,
+  gutterTop,
+  gutterBottom,
   theme,
   type,
   ...rest
 }: Props) => {
-  const Element = componentProp || typeElementMap[type] || "span";
+  const Element = elementProp || typeElementMap[type] || "span";
   return (
     <Element
-      className={classNames(classes.root, classes[type], className)}
+      className={classNames(
+        classes.root,
+        classes[type],
+        gutterTop && classes.gutterTop,
+        gutterBottom && classes.gutterBottom,
+        className
+      )}
       {...rest}
     >
       {children}
@@ -48,7 +70,7 @@ const Typography = ({
   );
 };
 
-const styles = (theme) => ({
+const styles = () => ({
   root: {
     width: "100%",
   },
@@ -56,12 +78,48 @@ const styles = (theme) => ({
     fontSize: "36px",
     lineHeight: "48px",
     fontWeight: "600",
-    color: theme.colors.gray800,
+    color: "#2E2E35",
+  },
+  heading2: {
+    fontSize: "32px",
+    lineHeight: "44px",
+    fontWeight: "600",
+    color: "#2E2E35",
+  },
+  heading3: {
+    fontSize: "28px",
+    lineHeight: "40px",
+    fontWeight: "600",
+    color: "#2E2E35",
+  },
+  heading4: {
+    fontSize: "24px",
+    lineHeight: "36px",
+    fontWeight: "600",
+    color: "#2E2E35",
+  },
+  heading5: {
+    fontSize: "20px",
+    lineHeight: "32px",
+    fontWeight: "600",
+    color: "#2E2E35",
+  },
+  heading6: {
+    fontSize: "18px",
+    lineHeight: "30px",
+    fontWeight: "600",
+    color: "#2E2E35",
   },
   body: {
     maxWidth: "600px",
     fontSize: "14px",
     lineHeight: "18px",
+  },
+  gutterBottom: {
+    marginBottom: "0.35em",
+  },
+  gutterTop: {
+    marginTop: "0.35em",
   },
 });
 
