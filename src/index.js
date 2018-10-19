9b27c573e492af3acdbad746bc6a7dcf33a82d49
Chris Heninger <chris.heninger@knledg.com>
2018-10-18 21:47:17 -0700

    Add export for Accordion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export { default as Accordion } from "./Accordion";
 export { default as Alert } from "./Alert";
 export { default as Button } from "./Button";
 export { default as Datetime } from "./Datetime";
