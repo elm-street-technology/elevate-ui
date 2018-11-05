bcb3a607e06dac64924a274e9683cf3dd3f9902b
Eric Stout <ericwstout@gmail.com>
2018-11-04 20:03:40 -0700

    Add gutters to HR component (#125)

--- src/Hr/index.js
+++ src/Hr/index.js
@@ -1,18 +1,28 @@
 // @flow
 import React, { Component } from "react";
 import withStyles from "../withStyles";
+import classNames from "classnames";
 
 type Props = {
   /**
    * Accepts any classNames to be passed to the component.
    */
   className: string,
+  classes: Object,
   /**
    * Color to be applied to the Hr component. Can be "primary", "secondary", or any custom value.
    */
-  color: string,
+  color?: string,
   /**
-   * Style object with any applicable overrides (common overrides are marginTop/marginBottom)
+   * Adds default margin bottom
+   */
+  gutterBottom?: boolean,
+  /**
+   * Adds default margin top
+   */
+  gutterTop?: boolean,
+  /**
+   * Style object with any applicable overrides
    */
   style: Object,
   /**
@@ -22,7 +32,7 @@ type Props = {
   /**
    * Height/thickness of the Hr component.
    */
-  thickness: string | number,
+  thickness?: string | number,
 };
 
 /**
@@ -30,18 +40,13 @@ type Props = {
  */
 class Hr extends Component<Props> {
   render() {
-    const { color, style, theme, thickness, ...rest } = this.props;
+    const { classes, className, style, ...rest } = this.props;
 
     return (
       <div
+        className={classNames(classes.root, className)}
         {...rest}
         style={{
-          display: "flex",
-          width: "100%",
-          height: thickness || "1px",
-          backgroundColor: theme.colors[color] || color || theme.colors.gray300,
-          marginTop: "16px",
-          marginBottom: "16px",
           ...style,
         }}
       />
@@ -49,6 +54,16 @@ class Hr extends Component<Props> {
   }
 }
 
-const styles = () => {};
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+    width: "100%",
+    height: (props) => props.thickness || "1px",
+    backgroundColor: (props) =>
+      theme.colors[props.color] || props.color || theme.colors.gray300,
+    marginTop: (props) => (props.gutterTop ? "1em" : 0),
+    marginBottom: (props) => (props.gutterBottom ? "1em" : 0),
+  },
+});
 
 export default withStyles(styles, { name: "EuiHr" })(Hr);
