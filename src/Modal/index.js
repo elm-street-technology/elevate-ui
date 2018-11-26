9efb3fe8d5a77d0a30a119e1a5c36a4df1a1f5eb
Eric Stout <ericwstout@gmail.com>
2018-11-26 10:21:17 -0700

    Update modal height on mobile

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -168,13 +168,16 @@ const styles = (theme) => ({
     justifyContent: "flex-start",
     alignItems: "center",
     margin: "0 auto",
-    minWidth: "100%",
+    minWidth: "95%",
     maxWidth: "85%",
-    maxHeight: "90vh",
+    maxHeight: "70vh",
     boxShadow:
       "0px 1px 5px 0px rgba(0, 0, 0, 0.2),0px 2px 2px 0px rgba(0, 0, 0, 0.14),0px 3px 1px -2px rgba(0, 0, 0, 0.12)",
     "@media (min-width: 450px)": {
       minWidth: "450px",
+    },
+    [theme.breakpoints[900]]: {
+      maxHeight: "90vh",
     },
   },
   dialog: {
