f8db4fae67eb6dcff2de2acd949fdde34e92aba6
Eric Stout <ericwstout@gmail.com>
2018-12-20 17:05:31 -0700

    Render accordion children when open to prevent network requests (#173)

--- demo/src/docs/Accordion.js
+++ demo/src/docs/Accordion.js
@@ -92,18 +92,15 @@ const items = [
     title: "Accordion Title 4",
     contents: (
       <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem elit,
-        commodo vitae hendrerit eu, luctus nec ipsum. Lorem ipsum dolor sit
-        amet, consectetur adipiscing elit. Etiam commodo, erat ut tempus
-        rhoncus, felis est dapibus odio, vitae vestibulum ligula libero in
-        magna. Aenean aliquet convallis feugiat. Nullam diam est, fringilla at
-        ex eget, molestie scelerisque augue. Nam nibh erat, tempor eu
-        pellentesque semper, auctor at ipsum. Proin tristique, sem sit amet
-        suscipit vulputate, libero tortor dignissim purus, et pretium tortor
-        lectus a orci. Phasellus imperdiet enim ligula, sit amet congue enim
-        tempor sed. Fusce pretium purus vel felis egestas venenatis. In et
-        lectus ut risus pulvinar porta. Sed a urna suscipit, posuere erat et,
-        volutpat dolor.
+        <iframe
+          title="exampleVideo"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/A0FZIwabctw"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
       </div>
     ),
   },
